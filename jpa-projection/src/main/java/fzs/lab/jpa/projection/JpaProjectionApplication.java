d56faff9a6fe36b2b82ca83737acdafdefebf7e9
Md. Farhan Zaman <farhanzamanarnob@gmail.com>
2024-02-02 11:09:37 +0600

    Created project

---
+++ jpa-projection/src/main/java/fzs/lab/jpa/projection/JpaProjectionApplication.java
@@ -0,0 +1,13 @@
+package fzs.lab.jpa.projection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaProjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaProjectionApplication.class, args);
+	}
+
+}
---
+++ jpa-projection/src/test/java/fzs/lab/jpa/projection/JpaProjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package fzs.lab.jpa.projection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaProjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
