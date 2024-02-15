b1206b53a562b669e2b65574acf342f8b5048f5b
Md. Farhan Zaman <farhanzamanarnob@gmail.com>
2024-02-15 16:44:13 +0600

    Update UserController.java

--- jpa-projection/src/main/java/fzs/lab/jpa/projection/api/UserController.java
+++ jpa-projection/src/main/java/fzs/lab/jpa/projection/api/UserController.java
@@ -8,10 +8,11 @@ import fzs.lab.jpa.projection.response.SuccessMessages;
 import fzs.lab.jpa.projection.service.UserService;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.validator.constraints.NotBlank;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
