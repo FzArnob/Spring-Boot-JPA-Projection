58dcf1dc7e873224ca39de2b597aeffbbeea40f2
Md. Farhan Zaman <farhanzamanarnob@gmail.com>
2024-02-16 10:17:07 +0600

    Role controller added

---
+++ jpa-projection/src/main/java/fzs/lab/jpa/projection/api/RoleController.java
@@ -0,0 +1,75 @@
+package fzs.lab.jpa.projection.api;
+
+import fzs.lab.jpa.projection.domain.Role;
+import fzs.lab.jpa.projection.response.RestResponse;
+import fzs.lab.jpa.projection.response.SuccessMessages;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/roles")
+public class RoleController {
+    @Getter
+    @Setter
+    public static class CreateRoleInput {
+        @NotBlank(message = "Role name is required")
+        private String name;
+    }
+
+    @Getter
+    @Setter
+    public static class UpdateRoleInput {
+        @NotNull(message = "Role Id is required")
+        private Long id;
+        @NotBlank(message = "Role name is required")
+        private String name;
+    }
+
+    private final RoleService roleService;
+
+    @Autowired
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @GetMapping
+    public ResponseEntity<RestResponse.SuccessResponse> getAllRoles() {
+        List<RoleDto> roles = roleService.getAllRoles();
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new RestResponse.SuccessResponse(SuccessMessages.ROLE_LIST.getMessage(roles.size()), roles));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RestResponse.SuccessResponse> getRoleById(@PathVariable Long id) throws RestException.ResourceNotFoundException {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new RestResponse.SuccessResponse(SuccessMessages.ROLE_DETAIL.getMessage(id), roleService.getRoleById(id)));
+    }
+
+    @PostMapping
+    public ResponseEntity<RestResponse.SuccessResponse> createRole(@RequestBody CreateRoleInput role) {
+        Role newRole = roleService.createRole(role);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new RestResponse.SuccessResponse(SuccessMessages.ROLE_CREATED.getMessage(newRole.getName()), newRole));
+    }
+
+    @PutMapping
+    public ResponseEntity<RestResponse.SuccessResponse> updateRole(@RequestBody UpdateRoleInput role) throws RestException.ResourceNotFoundException {
+        Role updatedRole = roleService.updateRole(role);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new RestResponse.SuccessResponse(SuccessMessages.ROLE_UPDATED.getMessage(updatedRole.getName()), updatedRole));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<RestResponse.SuccessResponse> deleteRole(@PathVariable Long id) throws RestException.ResourceNotFoundException {
+        roleService.softDeleteRole(id);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new RestResponse.SuccessResponse(SuccessMessages.ROLE_DELETED.getMessage(id), null));
+    }
+}
+
--- jpa-projection/src/main/java/fzs/lab/jpa/projection/response/SuccessMessages.java
+++ jpa-projection/src/main/java/fzs/lab/jpa/projection/response/SuccessMessages.java
@@ -5,8 +5,12 @@ public enum SuccessMessages {
     USER_UPDATED("Successfully updated user: %s"),
     USER_DETAIL("Successfully fetched user with id: %s"),
     USER_LIST("Successfully fetched total: %s users"),
-    USER_DELETED("Successfully deleted user with id: %s");
-
+    USER_DELETED("Successfully deleted user with id: %s"),
+    ROLE_CREATED("Successfully created role: %s"),
+    ROLE_UPDATED("Successfully updated role: %s"),
+    ROLE_DETAIL("Successfully fetched role with id: %s"),
+    ROLE_LIST("Successfully fetched total: %s roles"),
+    ROLE_DELETED("Successfully deleted role with id: %s");
     private final String message;
 
     SuccessMessages(String message) {
